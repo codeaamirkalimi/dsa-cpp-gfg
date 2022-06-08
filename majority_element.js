120d01811d460f3e5b7de68899af6ae95c61ee7f
Aamir Kalimi <codekalimi@gmail.com>
2022-06-08 14:08:46 +0530

    find majority element by using the loop and count

--- majority_element.js
+++ majority_element.js
@@ -45,7 +45,28 @@ function find_majority_element_moor_algorithm(arr) {
     return false;
 }
 
+
+function find_majority_element_loop(arr) {
+    arr.sort((a,b) => a-b)
+    let element = arr[0];
+    let count = 1;
+    for(let i = 1; i < arr.length; i++) {
+        if (arr[i] == element) {
+            count++;
+        } else {
+            count = 1;
+            element = arr[i];
+        }
+    }
+    if(count > parseInt(arr.length / 2)) {
+        return element;
+    }
+    return false;
+}
+
 console.log(majority_element([3, 3, 4, 2, 4, 4, 2, 4, 4]));
 console.log(majority_element([3, 3, 4, 2, 4, 4, 2, 4]));
 console.log(find_majority_element_moor_algorithm([3, 3, 4, 2, 4, 4, 2, 4, 4]));
 console.log(find_majority_element_moor_algorithm([3, 3, 4, 2, 4, 4, 2, 4]));
+console.log(find_majority_element_loop([3, 3, 4, 2, 4, 4, 2, 4, 4]));
+console.log(find_majority_element_loop([3, 3, 4, 2, 4, 4, 2, 4]));
