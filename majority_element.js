21a4f22d341f8b312b701140308c62b43a4fa546
Aamir Kalimi <codekalimi@gmail.com>
2022-06-08 13:31:01 +0530

    majority_element using moor's algorithm

--- majority_element.js
+++ majority_element.js
@@ -13,5 +13,39 @@ function majority_element(arr) {
     return false;
 }
 
+function findCandidate(arr) {
+    let majorityIndex = 0;
+    let count = 1;
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[majorityIndex] == arr[i]) {
+            count++;
+        } else {
+            count--;
+        }
+        if(count == 0) {
+            majorityIndex = i;
+            count = 1;
+        }
+    }
+    return arr[majorityIndex];
+}
+
+function find_majority_element_moor_algorithm(arr) {
+    let n = parseInt(arr.length / 2);
+    let element = findCandidate(arr);
+    let count = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] == element) {
+            count++;
+        }
+        if (count > n) {
+            return arr[i];
+        }
+    }
+    return false;
+}
+
 console.log(majority_element([3, 3, 4, 2, 4, 4, 2, 4, 4]));
 console.log(majority_element([3, 3, 4, 2, 4, 4, 2, 4]));
+console.log(find_majority_element_moor_algorithm([3, 3, 4, 2, 4, 4, 2, 4, 4]));
+console.log(find_majority_element_moor_algorithm([3, 3, 4, 2, 4, 4, 2, 4]));
